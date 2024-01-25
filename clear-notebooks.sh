b94c8d57aca6dc471ea9c9a890213320f54ad0c8
Julien Gaucher <julien.gaucher@csgroup.eu>
2024-01-25 17:33:43 +0100

    feat: script to clean notebooks before commiting

---
+++ clear-notebooks.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Clear jupyter notebook outputs (useful before commiting to git)
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+for notebook in $(find "$SCRIPT_DIR" -type f -name "*.ipynb"); do
+    (set -x; jupyter nbconvert --clear-output --inplace "$notebook")
+done
