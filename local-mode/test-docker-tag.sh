8e4c63c464b8d6946cdc636e798995cfd10f7753
Julien Gaucher <julien.gaucher@csgroup.eu>
2024-11-05 13:31:15 +0100

    Revert "debug why the ci/cd is not working"
    This reverts commit f00051400d5f14a503e367e6ed1e96885c023818.

--- local-mode/test-docker-tag.sh
+++ local-mode/test-docker-tag.sh
@@ -62,8 +62,7 @@ while IFS= read -r old_image ; do
 done <<< "$all_images"
 
 # Pull these images
-# (set -x; docker compose -f "$dc_file" --progress quiet pull)
-(set -x; docker compose -f "$dc_file" pull catalog-db postgres rs-server-catalog rs-server-cadip rs-server-adgs)
+(set -x; docker compose -f "$dc_file" --progress quiet pull)
 
 # Show usage
 echo -e "\nRun with:
