9dcebe12df3068a92c44382d2011aa7104806dbf
mcolinde <matthieu.colin-de-verdiere@csgroup.eu>
2024-12-04 09:54:05 +0100

    test: copy staging script inparent folders

---
+++ staging.py
@@ -0,0 +1,29 @@
+
+from prefect import flow, task
+#import rs_client
+
+
+
+@flow(name="staging")
+def staging():
+    """
+    Launch staging process
+    """
+    #staging.run_staging()
+    print("Launch staging flow...")
+
+
+
+if __name__ == "__main__":
+    
+    # Creates a deployment from your flow and immediately begins listening for scheduled runs to execute
+    #staging.serve(name="my-first-deployment", cron="* * * * *")
+    
+    # Create a deployment: here’s an example of a deployment that uses a work pool and bakes the code into a Docker image
+    staging.deploy(
+        name="my-second-deployment",
+        work_pool_name="my-work-pool",
+        image="my-image",
+        push=False,
+        cron="* * * * *",
+    )
