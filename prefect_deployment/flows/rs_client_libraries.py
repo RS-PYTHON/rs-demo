2f5ca4829cf97d7c25df040f3459d1e760ce5db1
Alex Grosu <agrosu@c-s.ro>
2025-01-14 20:59:03 +0200

    Some changes in the comments

--- prefect_deployment/flows/rs_client_libraries.py
+++ prefect_deployment/flows/rs_client_libraries.py
@@ -11,28 +11,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" Module that implements a prefect flow to be launched in a dask cluster """
+""" Module that implements a prefect flow that is launched in a prefect worker """
 import os
 from prefect import flow, get_run_logger, task
 import json
 from rs_client.staging_client import StagingClient
 
 @task
-def get_jobs(idx):
-    """Example task to be used in a prefect flow
-    The function simply multiplies two numbers
-    This prefect task may be used as start point in creating your own prefect tasks
+def get_jobs():
+    """Example task to be used in a prefect flow    
+    This prefect task may be used as start point in creating your own prefect tasks. It creates 
+    the rs-client-staging object used to call the rs-server-staging get_jobs endpoint only
 
     Args:
-        idx (int): Index of the task
-        x (int): First operator
-        y (int): Second operator
+        None
     """
     logger = get_run_logger()
-    logger.info(f"Running task get_jobs index {idx}")
+    logger.info("Running task get_jobs ")
     try:
-        rs_client_staging = StagingClient(os.environ["RS_SERVER_STAGING_ADDRESS"], 
-                                          os.environ["RS_API_KEY"], 
+        rs_client_staging = StagingClient(os.environ["RS_SERVER_STAGING_ADDRESS"],
+                                          os.environ["RS_API_KEY"],
                                           os.environ["RS_OWNER"], None)
     except KeyError:
         logger.exception(
@@ -40,28 +38,26 @@ def get_jobs(idx):
             )
         return "FAILED"
     response = rs_client_staging.get_jobs()
+
     resp = json.loads(response.content)
     logger.info(f"{resp}")
     return "OK"
     
 
 @flow(name="get_staging_jobs")
-def get_staging_jobs():    
+def get_staging_jobs():
     # Prefect flow and task definitions    
     
-    
-    """Example flow that will be launched to run in a dask cluster
+    """Flow that will be launched in a prefect worker
 
-    This prefect flow may be used as start point in creating your own prefect flows. It runs in parallel
-    add_numbers and multiply_numbers tasks
-
+    This prefect flow may be used as start point in creating your own prefect flows. It runs 
+    a task that creates the rs-client-staging object used to call the rs-server-staging endpoints
     Args:
-        name (str): Username to be printed. Default COPERNICUS
-        number_of_tasks (int): Number of tasks to be run. Default 5
+        None
     """
     logger = get_run_logger()
     logger.info("Running flow get_staging_jobs")
-    res = get_jobs.submit(1)
+    res = get_jobs.submit()
     
     logger.info(f"Task retunrned {res.result()}")    
     
