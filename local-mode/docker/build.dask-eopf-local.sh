302deb085c0b7c46828069db5bd0cb72d23e44bb
Julien Gaucher <julien.gaucher@csgroup.eu>
2025-02-07 10:28:36 +0100

    feat: build docker image for eopf manually

--- local-mode/docker/build.dask-eopf-local.sh
+++ local-mode/docker/build.dask-eopf-local.sh
@@ -22,11 +22,6 @@ DASK_GATEWAY_TAG="2024.1.0"
 PREFECT_TAG="3.1.4"
 EOPF_PIP_VERSION="2.5.2"
 
-set +x
-if [[ -z "${EOPF_TOKEN:-}" ]]; then
-    >&2 echo -e "usage: EOPF_TOKEN=*** $0\n(see: https://gitlab.eopf.copernicus.eu/help/user/profile/personal_access_tokens)"
-    exit 1
-fi
 set -x
 
 # Build the docker image
@@ -35,9 +30,9 @@ docker build \
     --build-arg "DASK_GATEWAY_TAG=${DASK_GATEWAY_TAG}" \
     --build-arg "PREFECT_TAG=${PREFECT_TAG}" \
     --build-arg "EOPF_PIP_VERSION=${EOPF_PIP_VERSION}" \
-    --secret id=EOPF_TOKEN \
     -f "${SCRIPT_DIR}/Dockerfile.dask-eopf-local" \
     -t "${registry}:${EOPF_PIP_VERSION}" \
+    -t "${registry}:latest" \
     --progress=plain \
     "$SCRIPT_DIR" \
 
