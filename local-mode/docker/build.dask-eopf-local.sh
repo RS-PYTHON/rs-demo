3df6bb1254c06876b0d35152c58261df91b7d5b0
Julien Gaucher <julien.gaucher@csgroup.eu>
2025-02-21 18:10:09 +0100

    feat: update the dask eopf docker image

--- local-mode/docker/build.dask-eopf-local.sh
+++ local-mode/docker/build.dask-eopf-local.sh
@@ -20,8 +20,12 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 DASK_GATEWAY_TAG="2024.1.0"
 PREFECT_TAG="3.1.4"
-EOPF_PIP_VERSION="2.5.2"
 
+set +x
+if [[ -z "${GITLAB_EOPF_TOKEN:-}" ]]; then
+    >&2 echo -e "usage: GITLAB_EOPF_TOKEN=*** $0\n(see: https://gitlab.eopf.copernicus.eu/help/user/profile/personal_access_tokens)"
+    exit 1
+fi
 set -x
 
 # Build the docker image
@@ -29,14 +33,12 @@ registry="ghcr.io/rs-python/dask-gateway-server/eopf/local"
 docker build \
     --build-arg "DASK_GATEWAY_TAG=${DASK_GATEWAY_TAG}" \
     --build-arg "PREFECT_TAG=${PREFECT_TAG}" \
-    --build-arg "EOPF_PIP_VERSION=${EOPF_PIP_VERSION}" \
+    --secret id=GITLAB_EOPF_TOKEN \
     -f "${SCRIPT_DIR}/Dockerfile.dask-eopf-local" \
-    -t "${registry}:${EOPF_PIP_VERSION}" \
     -t "${registry}:latest" \
     --progress=plain \
     "$SCRIPT_DIR" \
 
 # Push the images
 docker login https://ghcr.io/v2/rs-python
-docker push "${registry}:${EOPF_PIP_VERSION}"
 docker push "${registry}:latest"
