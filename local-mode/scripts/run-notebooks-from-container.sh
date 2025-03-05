08db8c0be3472d1c752865ec85d5ffc529eab0b8
Julien Gaucher <julien.gaucher@csgroup.eu>
2025-03-05 16:36:56 +0100

    feat: run l0 demo from ci/cd

--- local-mode/scripts/run-notebooks-from-container.sh
+++ local-mode/scripts/run-notebooks-from-container.sh
@@ -14,6 +14,11 @@ export RSPY_APPLY_STAGING_ENDPOINTS_VALIDATION=0
 # Read environment variables from the .env file
 source ${ROOT_DIR}/local-mode/.env
 
+# Override these values from the caller
+if [[ -n "${FORCE_RSPY_FROM_CICD:-}" ]]; then
+    export RSPY_FROM_CICD="$FORCE_RSPY_FROM_CICD"
+fi
+
 all_ok=
 all_errors=
 all_ignored=
@@ -24,6 +29,9 @@ for notebook in $(find $ROOT_DIR/notebooks -type f -name "*.ipynb" -not -path "*
     _dirname="$(dirname $notebook)"
     _filename="$(basename $notebook)"
     _relative="$(realpath $notebook --relative-to $ROOT_DIR)"
+
+    # For testing. Keep this line commented in git.
+    # if [[ "$_relative" != "notebooks/sprints/sprintxx/yyy.ipynb" ]]; then continue; fi
 
     # Ignore these notebooks
     if grep -q "$_relative" "${SCRIPT_DIR}/ignored-notebooks.txt"; then
