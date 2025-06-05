e5d6899f8523d3b6dd917912cf04823f5c24f80d
Julien Gaucher <julien.gaucher@csgroup.eu>
2025-06-05 18:33:48 +0200

    Run ci/cd from existing jupyter container

--- local-mode/scripts/run-notebooks-from-container.sh
+++ local-mode/scripts/run-notebooks-from-container.sh
@@ -4,37 +4,25 @@
 
 set -euo pipefail
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-ROOT_DIR="$(realpath $SCRIPT_DIR/../..)"
-
-# We are always on local mode
-export RSPY_LOCAL_MODE=1
-
 # This script is run from the ci/cd
 export RSPY_FROM_CICD=1
-
-# To import the resources/ folder
-export PYTHONPATH="${ROOT_DIR}/notebooks:${PYTHONPATH:-}"
-
-# Read environment variables from the .env file
-source ${ROOT_DIR}/local-mode/.env
 
 all_ok=
 all_errors=
 all_ignored=
 
 # For each demo notebook, sorted by name
-for notebook in $(find $ROOT_DIR/notebooks -type f -name "*.ipynb" -not -path "*checkpoints*" | sort); do
+for notebook in $(find "${HOME}/notebooks" -type f -name "*.ipynb" -not -path "*checkpoints*" | sort); do
 
     _dirname="$(dirname $notebook)"
     _filename="$(basename $notebook)"
-    _relative="$(realpath $notebook --relative-to $ROOT_DIR)"
+    _relative="$(realpath $notebook --relative-to $HOME)"
 
     # For testing. Keep this line commented in git.
     # if [[ "$_relative" != "notebooks/sprints/sprintxx/yyy.ipynb" ]]; then continue; fi
 
     # Ignore these notebooks
-    if grep -q "$_relative" "${SCRIPT_DIR}/ignored-notebooks.txt"; then
+    if grep -q "$_relative" "/scripts/ignored-notebooks.txt"; then
         all_ignored="${all_ignored:-}  - '$_relative'\n"
         continue
     fi
