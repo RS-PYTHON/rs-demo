71e4e16fcd363681403de7e95b29a310070808ad
Julien Gaucher <julien.gaucher@csgroup.eu>
2025-07-03 17:30:38 +0200

    Run notebooks as root

--- local-mode/run-notebooks.sh
+++ local-mode/run-notebooks.sh
@@ -25,4 +25,4 @@ wait_for_service 8888 "login" # jupyter
 
 # Run the notebooks from the jupyter service from the docker-compose.
 set -x;
-docker exec --privileged jupyter /scripts/run-notebooks-from-container.sh
+docker exec --user=root jupyter /scripts/run-notebooks-from-container.sh
