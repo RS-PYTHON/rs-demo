2fde90f36f596406f383cb6dc3e0a4d101c4ecba
Julien Gaucher <julien.gaucher@csgroup.eu>
2026-06-01 16:49:31 +0200

    Read JUPYTERHUB_API_TOKEN from Prefect blocks

--- notebooks/resources/dask_clusters/dask_utils.py
+++ notebooks/resources/dask_clusters/dask_utils.py
@@ -70,7 +70,11 @@ def get_dask_gateway(
             # NOTE: JUPYTERHUB_API_TOKEN is the token that was used to setup the Dask clusters.
             # It is saved and read in the Prefect block "env-vars".
             # This is not the JUPYTERHUB_API_TOKEN that is initialized automatically at the Jupyter session startup.
-            auth = JupyterHubAuth(os.environ["JUPYTERHUB_API_TOKEN"])
+            jupyter_token = os.environ["JUPYTERHUB_API_TOKEN"]
+            print(
+                f"JUPYTERHUB_API_TOKEN: '{jupyter_token[:8]}...' <- this common token is set in Prefect block",
+            )
+            auth = JupyterHubAuth(jupyter_token)
         except KeyError as error:
             raise KeyError(
                 "JUPYTERHUB_API_TOKEN environment variable is missing",
