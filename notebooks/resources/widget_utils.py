a7b1888b38ce01fc7c0d5cc34957ff3da7107d2b
Alin BUTU <alin.butu@cs-soprasteria.com>
2026-06-15 11:23:11 +0300

    update comment

--- notebooks/resources/widget_utils.py
+++ notebooks/resources/widget_utils.py
@@ -210,7 +210,7 @@ async def deploy_prefect(
             f"Deploy flows from {local_path!r} to 's3://{code_bucket.bucket_name}/{code_bucket.bucket_folder}'",
         )
 
-        # Upload local workflows package contents
+        # Upload local workflows package contents, then the client and config folders
         await code_bucket.put_directory(local_path=local_path, to_path="rs_workflows")
         await code_bucket.put_directory(
             local_path=osp.realpath(osp.join(local_path, "..", "rs_client")),
