5555de50d212b720c242d64dd58d3bfec634c907
Ovidiu <ovidiu.padeanu@cs-soprasteria.com>
2026-06-30 10:02:05 +0300

    Modified settings for OLCI dask cluster

--- notebooks/resources/dask_clusters/cluster_config/__init__.py
+++ notebooks/resources/dask_clusters/cluster_config/__init__.py
@@ -17,4 +17,5 @@
 from .dpr_container_config import *
 from .dpr_scheduler_pod_config import *
 from .dpr_worker_pod_config import *
+from .dpr_olci_worker_pod_config import *
 from .staging_pod_config import *
